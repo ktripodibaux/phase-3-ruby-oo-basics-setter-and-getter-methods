cf6f5a4f9d137c1c690f4b6f751e1db488907b99
Kurt Baux <ktripodibaux@gmail.com>
2022-07-19 19:43:00 -0400

    commit tree ef2cad31f0b2b4eddaa160c7c04b23856c50c416

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
 
+    attr_accessor :name, :breed
+
+    def name new_name
+        @name = new_name
+    end
+
+    def name
+        @name
+    end
+
+    def breed breed
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
+
 end
